830df123e353e08e49afc7603c66da8ea28d29a8
Манаков Владислав <vsmanakov@sberbank.ru>
2022-04-11 16:28:03 +0300

    Добавляет регистрацию service-worker.js

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import 'focus-visible/dist/focus-visible.min.js';
 import App from './App/index.svelte';
 import './styles/main.scss';
 
+window.addEventListener('load', () => {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('./service-worker.js');
+  }
+});
+
 const app = new App({
   target: document.getElementById('app') as HTMLElement
 });
